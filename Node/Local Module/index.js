fcef3f9db152c0ead3901a1536d0a850f35e6736
Prince Kumar <112944356+sinhaprince35@users.noreply.github.com>
2024-02-08 20:41:56 +0530

    Basic

--- Node/Local Module/index.js
+++ Node/Local Module/index.js
@@ -1,6 +1,6 @@
 console.log("This is index page");
 
-// Local Module
+// Local Module => The local module is a module created locally in your Nodejs application. It can be also called Custom or User-defined modules. In other words, Local modules are mainly used for specific projects that are locally available within the project folders.
 
 require("./add");
 
