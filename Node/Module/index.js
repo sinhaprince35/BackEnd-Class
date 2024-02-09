4d3cdcd4f1eddf055beb58bb726b9c6535d18642
Prince Kumar <112944356+sinhaprince35@users.noreply.github.com>
2024-02-09 08:22:14 +0530

    Node

---
+++ Node/Module/index.js
@@ -0,0 +1,19 @@
+/*
+Q.      What is module in NodeJS ?
+Ans :-  A module is a piece of reusable JavaScript code.
+        It could be a.js file or a directory containing.js files.
+        You can export the content of these files and use them in other files.
+
+=> Types of Node Modules
+There are three main types of Node modules
+1. Built-in modules (http, url, path, fs, os)
+   To include a module, use the require() function with the name of the module: 
+   Syntax :- let http = require('moduleName') 
+
+2. Local modules
+3. Third-party modules
+*/
+
+
+
+
