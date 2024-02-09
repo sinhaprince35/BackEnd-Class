c1c89f4485c8b942d9a327a15189dfe6c89699d9
Prince Kumar <112944356+sinhaprince35@users.noreply.github.com>
2024-02-09 08:02:15 +0530

    Node

--- Node/Local Module/add.js
+++
@@ -1,8 +0,0 @@
-const add = (a, b) => {
-    return a + b;
-}
-
-
-let total = add(5, 10);
-console.log("The sum of the number is: "+total);
-// console.log(add(2,4));
--- Node/Local Module/index.js
+++
@@ -1,8 +0,0 @@
-console.log("This is index page");
-
-/* Local Module => The local module is a module created locally in your Nodejs application. 
-It can be also called Custom or User - defined modules.In other words,
-Local modules are mainly used for specific projects that are locally available within the project folders. */
-
-require("./add");
-
